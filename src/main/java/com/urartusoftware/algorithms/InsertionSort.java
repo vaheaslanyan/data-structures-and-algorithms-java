74949e74433115c571351a39695954bb67d7755f
vaheaslanyan <vaheaslanyan@macbook1hunnid.local>
2023-02-04 16:50:40 -0600

    Add SelectionSort

---
+++ src/main/java/com/urartusoftware/algorithms/InsertionSort.java
@@ -0,0 +1,4 @@
+package com.urartusoftware.algorithms;
+
+public class InsertionSort {
+}
---
+++ src/main/java/com/urartusoftware/algorithms/SelectionSort.java
@@ -0,0 +1,22 @@
+package com.urartusoftware.algorithms;
+
+// Iterates through array and temporarily stores index of min value
+// after which the lowest item will be swapped to the begging of the array.
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if ( i != minIndex) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+}
